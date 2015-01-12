28cacce37719f57375208c1e982599bd23e4bebf
Joosep Pata <joosep.pata@cern.ch>
2015-01-12 13:59:37 +0200

    cleanup of treedumper samples

--- python/submitTreeDump.py
+++ python/submitTreeDump.py
@@ -34,147 +34,45 @@ xsecTT_FH = 112.33
 # /hdfs/cms/store/user/liis/TTH_Ntuples_v3/DiJetPt_ZZ_TuneZ2star_8TeV_pythia6_tauola.root
 
 ps = cms.VPSet([
-    cms.PSet(
-    	skip     = cms.bool(True),
-    	name     = cms.string('TTH_HToBB_M-125_8TeV-pythia6'),
-    	nickName = cms.string('TTH125'),
-    	color    = cms.int32(2),
-    	xSec     = cms.double(0.1302*0.569)
-    ),
-    cms.PSet(
-    	skip     = cms.bool(True),
-    	name     = cms.string('TTJets_SemiLeptMGDecays_8TeV-madgraph'),
-    	nickName = cms.string('TTJetsSemiLept'),
-    	color    = cms.int32(41),
-    	xSec     = cms.double(xsecTT_SL),
-    ),
-    cms.PSet(
-    	skip     = cms.bool(True),
-    	name     = cms.string('TTJets_FullLeptMGDecays_8TeV-madgraph'),
-    	nickName = cms.string('TTJetsFullLept'),
-    	color    = cms.int32(41),
-    	xSec     = cms.double(xsecTT_FL),
-    ),
-
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectronRun2012CAug24RerecoEdmV42'),
-    # nickName = cms.string('Run2012_DoubleElectronRun2012CAug24RerecoEdmV42'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectronRun2012D'),
-    # nickName = cms.string('Run2012_DoubleElectronRun2012D'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectron_Run2012A-13Jul2012-v1_ProcFIXED'),
-    # nickName = cms.string('Run2012_DoubleElectron_Run2012A-13Jul2012-v1_ProcFIXED'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectron_Run2012A-recover-06Aug2012-v1_ProcV2'),
-    # nickName = cms.string('Run2012_DoubleElectron_Run2012A-recover-06Aug2012-v1_ProcV2'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectron_Run2012B-13Jul2012-v1_ProcFIXED'),
-    # nickName = cms.string('Run2012_DoubleElectron_Run2012B-13Jul2012-v1_ProcFIXED'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectron_Run2012C-PromptReco-v2_HBB_EDMNtupleV42_ProcV1'),
-    # nickName = cms.string('Run2012_DoubleElectron_Run2012C-PromptReco-v2_HBB_EDMNtupleV42_ProcV1'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True),
-    # name     = cms.string('DoubleElectron_Run2012C-PromptReco-v2_HBB_EDMNtupleV42_ProcV2'),
-    # nickName = cms.string('Run2012_DoubleElectron_Run2012C-PromptReco-v2_HBB_EDMNtupleV42_ProcV2'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-
-
-
-    cms.PSet(
-    skip     = cms.bool(True),  # 148139
-    name     = cms.string('SingleMuRun2012AJul13'),
-    nickName = cms.string('SingleMuRun2012AJul13'),
-    color    = cms.int32(1),
-    xSec     = cms.double(-1),
-    ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 1551019
-    # name     = cms.string('SingleElectronRun2012AJul13EdmV42b'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012AJul13EdmV42b'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 9351330
-    # name     = cms.string('SingleElectronRun2012BJul13EdmV42'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012BJul13EdmV42'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 263593
-    # name     = cms.string('SingleElectronRun2012C-EcalRecover_11Dec2012-v1_v2'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012C-EcalRecover_11Dec2012-v1_v2'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 1064158
-    # name     = cms.string('SingleElectronRun2012CAug24RerecoEdmV42'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012CAug24RerecoEdmV42'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 9768094
-    # name     = cms.string('SingleElectronRun2012CPromptv2EdmV42'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012CPromptv2EdmV42'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 3491407
-    # name     = cms.string('SingleElectronRun2012CPromptV2TopUpEdmV42'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012CPromptV2TopUpEdmV42'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-    # cms.PSet(
-    # skip     = cms.bool(True), # 16178887
-    # name     = cms.string('SingleElectronRun2012D-PromptReco-v1_v3'),
-    # nickName = cms.string('Run2012_SingleElectronRun2012D-PromptReco-v1_v3'),
-    # color    = cms.int32(1),
-    # xSec     = cms.double(-1),
-    # ),
-
+	cms.PSet(
+		skip	 = cms.bool(True),
+		name	 = cms.string('TTH_HToBB_M-125_8TeV-pythia6'),
+		nickName = cms.string('TTH125'),
+		color	= cms.int32(2),
+		xSec	 = cms.double(0.1302*0.569)
+	),
+	cms.PSet(
+		skip	 = cms.bool(True),
+		name	 = cms.string('TTJets_SemiLeptMGDecays_8TeV-madgraph'),
+		nickName = cms.string('TTJetsSemiLept'),
+		color	= cms.int32(41),
+		xSec	 = cms.double(xsecTT_SL),
+	),
+	cms.PSet(
+		skip	 = cms.bool(True),
+		name	 = cms.string('TTJets_FullLeptMGDecays_8TeV-madgraph'),
+		nickName = cms.string('TTJetsFullLept'),
+		color	= cms.int32(41),
+		xSec	 = cms.double(xsecTT_FL),
+	),
+		cms.PSet(
+		skip	 = cms.bool(True),  # 148139
+		name	 = cms.string('SingleMuRun2012AJul13'),
+		nickName = cms.string('SingleMuRun2012AJul13'),
+		color	= cms.int32(1),
+		xSec	 = cms.double(-1),
+	),
 ])
 
 process = cms.Process("TreeProducer")
 
 process.fwliteInput = cms.PSet(
-    outFileName   = cms.string("PLACEHOLDER"),
-    pathToFile    = cms.string("/hdfs/cms/store/user/liis/TTH_Ntuples_v3/"),
-    ordering      = cms.string("DiJetPt_"),
-    samples = ps
+	outFileName = cms.string("PLACEHOLDER"), #replaced later
+	pathToFile = cms.string("/hdfs/cms/store/user/liis/TTH_Ntuples_v3/"),
+	ordering = cms.string("DiJetPt_"),
+	samples = ps
 )
+
 for i in range(len(ps)):
 	nick = ps[i].nickName.value()
 	process.fwliteInput.samples = cms.VPSet(ps[i])
